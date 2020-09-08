dd4e90fecad950ccacdf893fc861f34f74a2cbef
Sydney <s1811552@s.tsukuba.ac.jp>
2020-09-09 06:20:23 +0900

    finish chap2

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -9,36 +9,39 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
         VStack{
-            MapView()
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
             
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y:-130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text("Turtle rock")
+                Text(landmark.name)
                     .font(.title)
                     //.foregroundColor(.green)
                 
                 HStack(alignment: .top) {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
-                    Text("California").font(.subheadline)
+                    Text(landmark.state).font(.subheadline)
                 }
             }
             .padding()
             Spacer()
         }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }
