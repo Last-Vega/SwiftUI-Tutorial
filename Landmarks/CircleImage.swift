1cf85bc4dbc582609e364c8415751dcaf5ab97c6
Sydney <s1811552@s.tsukuba.ac.jp>
2020-09-09 04:46:14 +0900

    finish chap1

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 渡邉真悟−2 on 2020/09/09.
+//  Copyright © 2020 渡邉真悟. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 10)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 渡邉真悟−2 on 2020/09/08.
+//  Copyright © 2020 渡邉真悟. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle rock")
+                    .font(.title)
+                    //.foregroundColor(.green)
+                
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California").font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
