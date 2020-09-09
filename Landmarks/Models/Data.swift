81317ae8fafb929a27e801c1bed2e59c1a4e0549
Sydney <s1811552@s.tsukuba.ac.jp>
2020-09-09 15:48:18 +0900

    something fishy chap5

--- Landmarks/Models/Data.swift
+++ Landmarks/Models/Data.swift
@@ -6,18 +6,20 @@
 //  Copyright © 2020 渡邉真悟. All rights reserved.
 //
 
+import Foundation
+import CoreLocation
 import UIKit
 import SwiftUI
-import CoreLocation
 
 let landmarkData: [Landmark] = load("landmarkData.json")
+let hikeData: [Hike] = load("hikeData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-        else {
-            fatalError("Couldn't find \(filename) in main bundle.")
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
     
     do {
